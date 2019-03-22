20dfd7d407202e3d27cfb9f45969c4373a66eef9
ElsaQf <melody.qf@163.com>
2019-03-22 20:40:26 +0800

    逻辑回归

--- Code_Copy/004_Logistic_Regerssion.py
+++ Code_Copy/004_Logistic_Regerssion.py
@@ -45,6 +45,7 @@ plt.ylabel(' Estimated Salary')
 plt.legend()
 plt.show()
 
+from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, Y_test
 X1, X2 = np.meshgrid(np.arange(start=X_set[:,0].min()-1, stop=X_set[:,0].max()+1, step=0.01),
                      np.arange(start=X_set[:,1].min()-1, stop=X_set[:,1].max()+1, step=0.01))
@@ -55,7 +56,7 @@ plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
     plt.scatter(X_set[y_set==j,0], X_set[y_set==j,1],
                 c=ListedColormap(('red', 'green'))(i), label=j)
-
+    
 plt.title('LOGISTIC(Test set)')
 plt.xlabel('Age')
 plt.ylabel('Estimated Salary')
